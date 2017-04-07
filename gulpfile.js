60ae1eead7393341dfc9e5e967078585132e5d66
mlabouardy <mohamed@labouardy.com>
2017-04-07 11:14:40 +0200

    refactor

--- gulpfile.js
+++ gulpfile.js
@@ -8,60 +8,74 @@ var sourcemaps = require("gulp-sourcemaps");
 var concat = require("gulp-concat");
 var webserver = require('gulp-webserver');
 
+var input = {
+  sass : 'public/assets/scss/**/*.scss',
+  javascript : 'public/app/**/*.js',
+  html : 'public/**/*.html',
+  plugins : 'public/lib/*'
+}
+
+var output = {
+  css : 'build/assets/css',
+  javascript : 'build/assets/javascript',
+  plugins: 'build/lib'
+  build: 'build'
+}
+
 
 gulp.task("clean", function(cb){
   del(["build"], cb)
 })
 
 gulp.task('jshint', function(){
-  return gulp.src("public/app/**/*.js")
+  return gulp.src(input.javascript)
              .pipe(jshint())
              .pipe(jshint.reporter('jshint-stylish'))
 })
 
 gulp.task("build-css", function(){
-  return gulp.src("public/assets/scss/*.scss")
+  return gulp.src(input.sass)
              .pipe(sass().on("error", sass.logError))
-             .pipe(gulp.dest("build/assets/css"))
+             .pipe(gulp.dest(output.css))
 })
 
 gulp.task("build-js", function(){
-  return gulp.src("public/app/**/*.js")
+  return gulp.src(input.javascript)
              .pipe(sourcemaps.init())
              .pipe(concat("bundle.js"))
              .pipe(uglify())
              .pipe(sourcemaps.write())
-             .pipe(gulp.dest("build/assets/javascript"))
+             .pipe(gulp.dest(output.javascript))
 });
 
 gulp.task("build-html", function(){
-  return gulp.src(["public/**/*.html"])
-             .pipe(gulp.dest("build"))
+  return gulp.src([input.html])
+             .pipe(gulp.dest(output.build))
 });
 
 gulp.task("build-plugins", function(){
-  return gulp.src(["public/lib/*"])
-             .pipe(gulp.dest("build/lib"))
+  return gulp.src([input.plugins])
+             .pipe(gulp.dest(output.lib))
 })
 
 gulp.task("inject", function(){
-  var sources = gulp.src(["build/assets/**/*.js", "build/assets/**/*.css"], {read:false})
-  return gulp.src("build/index.html")
-            .pipe(inject(sources, {ignorePath:'build'}))
-            .pipe(gulp.dest("build"))
+  var sources = gulp.src([output.javascript, output.css], {read:false})
+  return gulp.src(output.build + "/index.html")
+            .pipe(inject(sources, {ignorePath: output.build}))
+            .pipe(gulp.dest(output.build))
 })
 
 gulp.task("watch", function(){
-  gulp.watch(["public/**/*.scss"], ["build-css"]).on("change", function(e){
+  gulp.watch([input.scss], ["build-css"]).on("change", function(e){
     console.log("SASS file " + e.path + " has been compiled !")
   })
 
-  gulp.watch(["public/app/**/*.js"], ["jshint", "build-js"]).on("change", function(e){
+  gulp.watch([input.javascript], ["jshint", "build-js"]).on("change", function(e){
     console.log("Running jshint analyser on " + e.path)
     console.log("Building JS file " + e.path)
   })
 
-  gulp.watch(["public/app/**/*.html"], ["build-html"]).on("change", function(e){
+  gulp.watch([input.html], ["build-html"]).on("change", function(e){
     console.log("Building html file " + e.path)
   })
 })
